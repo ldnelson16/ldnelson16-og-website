b16da865e473701d529cdc14322f4ff31302aa9c
Luke Nelson <ldnelson16@gmail.com>
2023-07-11 22:03:21 -0400

    Working on using backend cheerio to create webpages

--- js/backend.js
+++ js/backend.js
@@ -1,5 +1,6 @@
 import readline from "readline";
 import fs from "fs";
+import cheerio from "cheerio";
 //import FileReader from "FileReader";
 
 function readFileLines(filename) {
@@ -24,19 +25,52 @@ function readFileLines(filename) {
     });
   });
 }
+function fillHeader($,type,text){
+  try {
+    $("#header").append($(`<`+type+`>`+text+`</`+type+`>`));
+  }
+  catch (error) {
+    console.log("Caught error filling header with "+text);
+  }
+  return $;
+}
+function writeHTMLSingleColumnFrameworkFile(frameworkfilepath) {
+  const html = fs.readFileSync(frameworkfilepath,"utf-8");
+  const $ = cheerio.load(html);
+  return $;
+}
+function writeFile(filename,$){
+  //Converting html from Cheerio object to String
+  const stringHTML = $.html();
+  //Writing file to the given filename
+  fs.writeFile(filename,stringHTML,err => {
+    if (err) {
+      console.error('Error writing to '+filename, err);
+    } else {
+      console.log(filename+' file has been successfully created.');
+    }
+  });
+}
+function addDatesDropdown($,dates,labeltext,dropdownid,dropdownlabelid) {
+  const appendabledropdown = `<label for="`+dropdownid+`" id="`+dropdownlabelid+`">`+labeltext+`</label><select id = "`+dropdownid+`"></select>`;
+  console.log(appendabledropdown);
+  $("#body").append($(appendabledropdown));
+  for(let date in dates){
+    $("#"+dropdownid).append(`<option value="`+date+`">`+date+`</option>`);
+  }
+}
 
+
+
+
+//IMPLEMENTATION OF FILE
+let $ = writeHTMLSingleColumnFrameworkFile("singlecolumnframework.html");
+$=fillHeader($,"h2","H2");
 let d=readFileLines("data.txt").then(
   (lines) => {
-    console.log(lines[0]);
-    let dates=lines[0];
-    fs.writeFile('testingindex.html', lines[0], err => {
-      if (err) {
-        console.error('Error writing to index.html:', err);
-      } else {
-        console.log('testingindex.html file has been created.');
-      }
-    });
+    let dates=lines[0].split(" ");
+    console.log(dates);
+    addDatesDropdown($,dates,"Label","dropdown","dropdownlabel");
   }
 );
-console.log(d);
-//console.log(readFileLines("data.txt"));
+writeFile("newtestingrecruiting.html",$);
