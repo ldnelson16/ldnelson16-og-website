375b1ed4e02720d3acac5c4cd69eec96d051585d
Luke Nelson <ldnelson16@gmail.com>
2023-07-12 22:49:19 -0400

    Trying to make dropdown js work

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,21 @@ function loadDropdownOptions() {
   dropdown.add(option);
   console.log("date added")
 }
+function changeTableColumns(){
+  console.log("Changing Table Columns");
+  const dropdown = document.getElementById('dropdown');
+  const table = document.getElementById('tableid');
+  const selectedValue = dropdown.value;
+  const dropdownindex = dropdown.selectedIndex;
+  console.log(selectedValue,dropdownindex);
+  const length = dropdown.length;
+  const indexes = [dropdownindex+1,dropdownindex+1+length,dropdownindex+1+2*length,dropdownindex+1+3*length];
+  console.log(indexes);
+
+  // Hide all cells in the column
+  for (let i = 0; i < table.rows.length; i++) {
+    for (let j=0;j<indexes.length;++j){
+      table.rows[i].cells[indexes[j]].style.display = 'none';
+    }
+  }
+}
